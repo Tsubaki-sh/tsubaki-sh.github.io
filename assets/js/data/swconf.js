8a02ba9d6572973ef8334969765397b7fcc9fe17
Tsubaki_sh <Tsubaki_sh@outlook.com>
2026-09-13 21:22:41 +0800

    标题修复

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1773064269',resources: [
+    cacheName: 'chirpy-1786978670',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
